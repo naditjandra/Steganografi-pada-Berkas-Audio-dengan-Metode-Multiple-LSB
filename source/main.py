aa89a9675bde877ee8d08672e22001b595dad6d0
naditjandra <168246764+naditjandra@users.noreply.github.com>
2025-10-21 22:40:40 +0700

    final main

--- source/main.py
+++ source/main.py
@@ -62,4 +62,9 @@ def main():
         print("Invalid choice. Please enter 1 or 2.")
 
 if __name__ == "__main__":
-    main()
+    running = True
+    while running:
+        main()
+        quit = input("Quit Program? (y/n): ").strip()
+        if quit == 'y':
+            break
